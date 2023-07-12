50471487a6d637e043536ca4b0e4c00a4797daa2
Archan Bhatta <archanme1@gmail.com>
2023-07-11 21:51:19 -0400

    secured mongo key

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log("database connected");
+    console.log("database connected.");
   } catch (error) {
     console.log("connect failed database");
   }
